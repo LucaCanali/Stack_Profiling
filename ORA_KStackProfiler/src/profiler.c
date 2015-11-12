74b7a34d842c3a796907fcbef6ca50d76c39cf6c
Luca Canali <luca.canali@cern.ch>
2015-11-12 20:08:02 +0100

    v 1.0

--- ORA_KStackProfiler/src/profiler.c
+++ ORA_KStackProfiler/src/profiler.c
@@ -3,6 +3,8 @@
 #include <sys/syscall.h>
 #include <sys/uio.h>
 
+char process_status[128];
+
 /* reads selected values from Oracle process memory (specificallay from X$KSUSE in SGA) */
 /* for old kernels read from /proc/<pid>/mem should be used instead of SYS_process_vm_readv */
 int read_ksuse(int pid, int *ksuseopc_addr, int *ksusetim_addr)
@@ -33,19 +35,63 @@ int read_ksuse(int pid, int *ksuseopc_addr, int *ksusetim_addr)
 
     ksuseopc=*(int *) buf1;
     ksusetim=*(int *) buf2;
-    
+
  /* X$KSUSE.ksusetim is the time in microseconds since last wait. 
- *  *  When X$KSUSE.ksusetim >0 the session is not waiting 
- *   *  ignore edge cases of sessions transitioning state, 
- *    *  i.e. X$KSUSE.ksusetim=0 (musec) and process just switched on CPU 
- *     *  This is a workaround as one should rather check X$KSLWT.KSLWTINWAIT
- *      */
+ *  When X$KSUSE.ksusetim >0 the session is not waiting 
+ *  ignore edge cases of sessions transitioning state, 
+ *  i.e. X$KSUSE.ksusetim=0 (musec) and process just switched on CPU 
+ *  This is a workaround as one should rather check X$KSLWT.KSLWTINWAIT
+ */
+
     if (ksusetim>0) {
         return 0; /* on CPU */
     } else {
         return ksuseopc; /*wait event number */
     }
     
+}
+
+int gather_proc_status(int pid)  
+{
+    FILE *file;
+    char file_name[128];
+    int len;
+
+    sprintf(file_name, "/proc/%d/status", pid);
+    
+    if ((file = fopen(file_name, "r")) == NULL) {
+        fprintf(stderr, "cannot open %s\n", file_name);
+        return -1;
+    }
+
+    len = sizeof(process_status);
+    fgets(process_status, len, file);
+    fgets(process_status, len, file); /* we are interested in line N.2 */
+
+    fclose(file);
+    return 0;
+}
+
+int print_kernel_stack(int pid) 
+{
+    FILE *file;
+    char file_name[128];
+    char addr[512];
+    char name[512];
+
+    sprintf(file_name, "/proc/%d/stack", pid);
+ 
+    if ((file = fopen(file_name, "r")) == NULL) {
+        fprintf(stderr, "cannot open %s\n", file_name);
+        return -1;
+    }
+    
+    while (fscanf(file,"%s %s", addr, name) !=EOF)
+        printf("%s\n", name);
+
+    fclose(file);
+
+    return 0;
 }
 
 /* stack profiling: kernel stack, OS state and optionally Oracle wait event info from SGA */
@@ -56,41 +102,44 @@ int stack_profile(int pid, int sleep_micro, int num_samples, int *ksuseopc_addr,
    char command_stack[128];
    char command_status[128];
    
-   sprintf(command_stack, "cat /proc/%d/stack", pid);
-   sprintf(command_status, "grep -m 1 State /proc/%d/status", pid);
+   sprintf(command_stack, "cat /proc/%d/stack > /dev/null", pid);
+   sprintf(command_status, "cat /proc/%d/status > /dev/null", pid);
       
    for (i=0; i < num_samples; i++) {
 
         /* note, data collection is not atomic, this can introduce errors */
 
-        /* spawning reads of /proc/pid from external shell processes seems more accurate than  */
-        /* reading using a function in this module. Yet to be understood as I'd prefer to keep */
-        /* all into this C program.                                                            */
-        if (system(command_stack) != 0)   /* read kernel stack */
-            exit(-1);
-        if (system(command_status) != 0)  /* read process state */
-            exit(-1);
-       
-        /* optionally read Oracle wait event info if the addresses are provided */
-        if ((ksuseopc_addr != NULL) &&  (ksusetim_addr != NULL)) {
+        if (gather_proc_status(pid) < 0)
+            return -1;
 
+        /* optionally read Oracle wait event info if the addresses are provided */
+        if ((ksuseopc_addr != NULL) &&  (ksusetim_addr != NULL)) 
             if ((event = read_ksuse(pid, ksuseopc_addr, ksusetim_addr)) < 0) 
                 return -1;
 
+        if (print_kernel_stack(pid) < 0)
+            return -1;        
+        
+        puts(process_status);
+        
+        if ((ksuseopc_addr != NULL) &&  (ksusetim_addr != NULL))    
             if (event > 0) 
                 printf("event#=%d (Oracle state)\n", event);
             else 
-                printf("On CPU (Oracle state)\n");          
-                            
-        }
+                printf("On CPU (Oracle state)\n");                          
 
         /* this is needed to post process the output with FlameGraph/stackcollapse-stap.pl */   
         printf("1\n\n");
-        fflush(stdout);
+        
+        /* spawning reads of /proc/pid from external shell processes seems to improve accuracy  */
+        /* This is a workaround, yet to be understood for a future version */
+        if (system(command_stack) != 0)   /* read kernel stack */
+            exit(-1);
+        if (system(command_status) != 0)  
+            exit(-1);
 
         usleep(sleep_micro);
-    }
-            
+    }       
     return 0;
 }
 
--- Ptrace_Profiler/src/profiler.c
+++ Ptrace_Profiler/src/profiler.c
@@ -3,50 +3,7 @@
 #include <sys/syscall.h>
 #include <sys/uio.h>
 
-double cpu_time_used;
-
-int print_kernel_stack(int pid) 
-{
-    FILE *file;
-    char file_name[128];
-    char addr[512];
-    char name[512];
-
-    sprintf(file_name, "/proc/%d/stack", pid);
- 
-    if ((file = fopen(file_name, "r")) == NULL) {
-        fprintf(stderr, "cannot open %s\n", file_name);
-        return -1;
-    }
-    
-    while (fscanf(file,"%s %s", addr, name) !=EOF)
-        printf("%s\n", name);
-
-    fclose(file);
-
-    return 0;
-}
-
-char proc_stat(int pid)  
-{
-    FILE *file;
-    char buf[128];
-    char pidstring[128];
-    char pname[128];
-    char pstate[1];
-    
-    sprintf(buf, "/proc/%d/stat", pid);
-    if ((file = fopen(buf, "r")) == NULL) {
-        fprintf(stderr, "Cannot open %s\nNo permissions or pid=%d does not esist\n\n", buf, pid);
-        exit(-1);
-    }
-
-    fscanf(file, "%s %s %s", pidstring, pname, pstate);
-
-    fclose(file);
-    return toupper(pstate[0]);
-}
-
+char process_status[128];
 
 /* reads selected values from Oracle process memory (specificallay from X$KSUSE in SGA) */
 /* for old kernels read from /proc/<pid>/mem should be used instead of SYS_process_vm_readv */
@@ -78,81 +35,127 @@ int read_ksuse(int pid, int *ksuseopc_addr, int *ksusetim_addr)
 
     ksuseopc=*(int *) buf1;
     ksusetim=*(int *) buf2;
-    
-    /* X$KSUSE.ksusetim is the time in microseconds since last wait. 
- *        When X$KSUSE.ksusetim >0 the session is not waiting 
- *               ignore edge cases of sessions transitioning state, 
- *                      i.e. X$KSUSE.ksusetim=0 (musec) and process just switched on CPU 
- *                             This is a workaround as one should rather check X$KSLWT.KSLWTINWAIT
- *                                  */
+
+ /* X$KSUSE.ksusetim is the time in microseconds since last wait. 
+ *  When X$KSUSE.ksusetim >0 the session is not waiting 
+ *  ignore edge cases of sessions transitioning state, 
+ *  i.e. X$KSUSE.ksusetim=0 (musec) and process just switched on CPU 
+ *  This is a workaround as one should rather check X$KSLWT.KSLWTINWAIT
+ */
     if (ksusetim>0) {
-        return 0; /* on CPU */
+        return 0;        /* On CPU */
     } else {
-        return ksuseopc; /*wait event number */
+        return ksuseopc; /* Wait event number */
     }
     
 }
 
+int gather_proc_status(int pid)  
+{
+    FILE *file;
+    char file_name[128];
+    int len;
+
+    sprintf(file_name, "/proc/%d/status", pid);
+    
+    if ((file = fopen(file_name, "r")) == NULL) {
+        fprintf(stderr, "cannot open %s\n", file_name);
+        return -1;
+    }
+
+    len = sizeof(process_status);
+    fgets(process_status, len, file);
+    fgets(process_status, len, file); /* we are interested in line N.2 */
+
+    fclose(file);
+    return 0;
+}
+
+int print_kernel_stack(int pid) 
+{
+    FILE *file;
+    char file_name[128];
+    char addr[512];
+    char name[512];
+
+    sprintf(file_name, "/proc/%d/stack", pid);
+ 
+    if ((file = fopen(file_name, "r")) == NULL) {
+        fprintf(stderr, "cannot open %s\n", file_name);
+        return -1;
+    }
+    
+    while (fscanf(file,"%s %s", addr, name) !=EOF)
+        printf("%s\n", name);
+
+    fclose(file);
+
+    return 0;
+}
+
+/* stack profiling: kernel stack, OS state and optionally Oracle wait event info from SGA */
 int stack_profile(int pid, int sleep_micro, int num_samples, int *ksuseopc_addr, int *ksusetim_addr)
 {
    int i;
-   char procstate;
    int event;
    char command_stack[128];
    char command_status[128];
    
-   sprintf(command_stack, "cat /proc/%d/stack", pid);
-   sprintf(command_status, "grep -m 1 State /proc/%d/status", pid);
+   sprintf(command_stack, "cat /proc/%d/stack > /dev/null", pid);
+   sprintf(command_status, "cat /proc/%d/status > /dev/null", pid);
    
-    if (unwind_initialize(pid) < 0)  {
-        detach_process(pid);
+    if (unwind_initialize(pid) < 0)  
         return -1;
-    }
-	
-    for (i=0; i < num_samples; i++) {
+
+   for (i=0; i < num_samples; i++) {
 
         /* note, data collection is not atomic, this can introduce errors */
 
-        if (system(command_stack) != 0)   /* read kernel stack */
-            exit(-1);
-			
-		if (attach_process(pid) < 0)
+        if (gather_proc_status(pid) < 0)
             return -1;
 
-		if (unwind(pid) < 0)  {
+        /* optionally read Oracle wait event info if the addresses are provided */
+        if ((ksuseopc_addr != NULL) &&  (ksusetim_addr != NULL)) 
+            if ((event = read_ksuse(pid, ksuseopc_addr, ksusetim_addr)) < 0) 
+                return -1;
+
+        if (print_kernel_stack(pid) < 0)
+            return -1;        
+        
+        if (attach_process(pid) < 0)
+            return -1;
+
+        if (unwind(pid) < 0)  {
            detach_process(pid);
            return -1;
         }
 
         if (detach_process(pid) < 0)
            return -1;
-	  
-        fflush(stdout);
-        if (system(command_status) != 0)  /* read process state */
-            exit(-1);
-       
-        /* optionally read Oracle wait event info if the addresses are provided */
-        if ((ksuseopc_addr != NULL) &&  (ksusetim_addr != NULL)) {
-
-            if ((event = read_ksuse(pid, ksuseopc_addr, ksusetim_addr)) < 0) 
-                return -1;
-
+      
+        puts(process_status);
+        
+        if ((ksuseopc_addr != NULL) &&  (ksusetim_addr != NULL))    
             if (event > 0) 
                 printf("event#=%d (Oracle state)\n", event);
             else 
-                printf("On CPU (Oracle state)\n");          
-                            
-        }
-		
+                printf("On CPU (Oracle state)\n");                          
+
+        /* this is needed to post process the output with FlameGraph/stackcollapse-stap.pl */   
         printf("1\n\n");
-        fflush(stdout);
-		
+        
+        /* spawning reads of /proc/pid from external shell processes seems to improve accuracy  */
+        /* This is a workaround, yet to be understood for a future version */
+        if (system(command_stack) != 0)   /* read kernel stack */
+            exit(-1);
+        if (system(command_status) != 0)  
+            exit(-1);
+
         usleep(sleep_micro);
-    }
-            
-    unwind_cleanup();
+    }      
+
+    unwind_cleanup();   
     return 0;
 }
-
 
 
